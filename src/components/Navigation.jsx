9b4996b04febad28e47dc997145bdcf0196eee0f
henryk89 <henrykutrieb@gmail.com>
2023-01-20 16:27:12 -0700

    All finished sections on notion added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -208,7 +208,6 @@ export const navigation = [
     links: [
       { title: 'Migrating from Gumroad', href: '/gumroad' },
       { title: 'Migrating from Launchpass', href: '/launchpass' },
-      { title: 'Migrating from Paddle', href: '/paddle' },
     ],
   },
   {
@@ -232,16 +231,16 @@ export const navigation = [
       { title: 'Graphic Design / VFX Services', href: '/graphic' },
     ],
   },
-  {
-    title: 'Software',
-    links: [
-      { title: 'Getting Started', href: '/software-intro' },
-      { title: 'Webhook Integration', href: '/webhook' },
-      { title: 'API Integration', href: '/software-api' },
-      { title: 'Available templates to get you started', href: '/templates' },
-      { title: 'Software FAQ', href: '/software-faq' },
-    ],
-  },
+  // {
+  //   title: 'Software',
+  //   links: [
+  //     { title: 'Getting Started', href: '/software-intro' },
+  //     { title: 'Webhook Integration', href: '/webhook' },
+  //     { title: 'API Integration', href: '/software-api' },
+  //     { title: 'Available templates to get you started', href: '/templates' },
+  //     { title: 'Software FAQ', href: '/software-faq' },
+  //   ],
+  // },
   {
     title: 'Discord Communities',
     links: [
@@ -249,19 +248,25 @@ export const navigation = [
     ],
   },
   {
-    title: 'Courses',
+    title: 'Templates',
     links: [
-      { title: 'Start selling', href: '/courses-intro' },
-      { title: 'Courses FAQ', href: '/courses-faq' },
+      { title: 'Start selling', href: '/templates-intro' },
     ],
   },
-  {
-    title: 'Files',
-    links: [
-      { title: 'Start selling', href: '/files-intro' },
-      { title: 'Files FAQ', href: '/files-faq' },
-    ],
-  },
+  // {
+  //   title: 'Courses',
+  //   links: [
+  //     { title: 'Start selling', href: '/courses-intro' },
+  //     { title: 'Courses FAQ', href: '/courses-faq' },
+  //   ],
+  // },
+  // {
+  //   title: 'Files',
+  //   links: [
+  //     { title: 'Start selling', href: '/files-intro' },
+  //     { title: 'Files FAQ', href: '/files-faq' },
+  //   ],
+  // },
   {
     title: 'Marketplace',
     links: [
@@ -270,49 +275,48 @@ export const navigation = [
       { title: 'Marketplace FAQ', href: '/marketplace-faq' },
     ],
   },
-  {
-    title: 'Integrations',
-    links: [
-      { title: 'Mailmodo', href: '/mailmodo' },
-      { title: 'Push Notifications', href: '/notifications' },
-      { title: 'Embeds', href: '/embeds' },
-      { title: 'Graphic Design / VFX Services', href: '/graphic' },
-    ],
-  },
-  {
-    title: 'Additional Migrations',
-    links: [
-      { title: 'Migrating from a Custom Dashboard', href: '/custom' },
-      { title: 'Migrating from a Patreon', href: '/patreon' },
-      { title: 'Migrating from a UpgradeChat', href: '/upgradechat' },
-      { title: 'Migrating from a Hyper', href: '/hyper' },
-      { title: 'Migrating from a Payrole', href: '/payrole' },
-      { title: 'Migrating from a Memberful', href: '/memberful' },
-    ],
-  },
+  // {
+  //   title: 'Integrations',
+  //   links: [
+  //     { title: 'Mailmodo', href: '/mailmodo' },
+  //     { title: 'Push Notifications', href: '/notifications' },
+  //     { title: 'Embeds', href: '/embeds' },
+  //     { title: 'Graphic Design / VFX Services', href: '/graphic' },
+  //   ],
+  // },
+  // {
+  //   title: 'Additional Migrations',
+  //   links: [
+  //     { title: 'Migrating from a Custom Dashboard', href: '/custom' },
+  //     { title: 'Migrating from a Patreon', href: '/patreon' },
+  //     { title: 'Migrating from a UpgradeChat', href: '/upgradechat' },
+  //     { title: 'Migrating from a Hyper', href: '/hyper' },
+  //     { title: 'Migrating from a Payrole', href: '/payrole' },
+  //     { title: 'Migrating from a Memberful', href: '/memberful' },
+  //   ],
+  // },
   {
     title: 'Tokengating',
     links: [
       { title: 'Getting started', href: '/tokengating-intro' },
-      { title: 'Tokengating FAQ', href: '/tokengating-faq' },
     ],
   },
-  {
-    title: 'Stats',
-    links: [
-      { title: 'MRR', href: '/mrr' },
-      { title: 'Net revenue', href: '/net-revenue' },
-    ],
-  },
-  {
-    title: 'Storefront',
-    links: [
-      { title: 'Store front vs. product page', href: '/store-vs-product' },
-      { title: 'Editing your store', href: '/editing' },
-      { title: 'Embeds', href: '/embeds' },
-      { title: 'Graphic Design / VFX Services', href: '/graphic' },
-    ],
-  },
+  // {
+  //   title: 'Stats',
+  //   links: [
+  //     { title: 'MRR', href: '/mrr' },
+  //     { title: 'Net revenue', href: '/net-revenue' },
+  //   ],
+  // },
+  // {
+  //   title: 'Storefront',
+  //   links: [
+  //     { title: 'Store front vs. product page', href: '/store-vs-product' },
+  //     { title: 'Editing your store', href: '/editing' },
+  //     { title: 'Embeds', href: '/embeds' },
+  //     { title: 'Graphic Design / VFX Services', href: '/graphic' },
+  //   ],
+  // },
 ]
 
 export function Navigation(props) {
