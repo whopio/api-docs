e25e7dcbe5416b2fc127842b3a747f11eab33970
henryk89 <henrykutrieb@gmail.com>
2023-01-26 13:39:40 -0700

    Google analytics and updated meta tags

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { Router, useRouter } from 'next/router'
 import { MDXProvider } from '@mdx-js/react'
-
+import { Script } from 'next/script'
 import { Layout } from '../components/Layout'
 import * as mdxComponents from '../components/mdx'
 import { useMobileNavigationStore } from '../components/MobileNavigation'
@@ -28,7 +28,8 @@ export default function App({ Component, pageProps }) {
         ) : (
           <title>{`${pageProps.title} - Whop Docs`}</title>
         )}
-        <meta name="description" content={pageProps.description} />
+        <meta name="description" content="This documentation is intended to give you an overview of Whop’s business features, our dashboards flexibility, and provide tips for how to use Whop to streamline your businesses reach, fulfillment, workflow and growth." />
+        <meta property="og:image" content={pageProps.image || 'https://i.imgur.com/mcMNF2o.jpg'} />
       </Head>
       <MDXProvider components={mdxComponents}>
         <Layout {...pageProps}>
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -39,10 +39,35 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=GTM-M8QQDFQ`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'GTM-M8QQDFQ', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         <script dangerouslySetInnerHTML={{ __html: modeScript }} />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
       </Head>
       <body className="bg-white antialiased dark:bg-zinc-900">
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8QQDFQ"
+      height="0" width="0" className='hidden invisible'></iframe></noscript>
         <Main />
         <NextScript />
       </body>
